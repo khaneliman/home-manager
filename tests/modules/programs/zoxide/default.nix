466f4f1c4c6b1fbcacc6255041395d11ee5cf979
Austin Horstman <khaneliman12@gmail.com>
2025-07-14 22:28:22 -0500

    tests/zoxide: add comprehensive tests
    - Add bash integration test for zoxide shell integration
    - Add fish integration test with source command
    - Add zsh integration test with eval command
    - Add nushell integration test with source command
    - Add options test with --no-cmd and --hook flags
    - Test covers all shell integrations and configuration options
    
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

---
+++ tests/modules/programs/zoxide/default.nix
@@ -0,0 +1,7 @@
+{
+  zoxide-bash-integration = ./bash-integration.nix;
+  zoxide-fish-integration = ./fish-integration.nix;
+  zoxide-zsh-integration = ./zsh-integration.nix;
+  zoxide-nushell-integration = ./nushell-integration.nix;
+  zoxide-with-options = ./with-options.nix;
+}
