eefd9dc3f42910a62ff92b5a225500ad0f71a13b
Austin Horstman <khaneliman12@gmail.com>
2025-09-05 19:16:36 -0500

    WIP
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

--- tests/modules/programs/jq/null-package.nix
+++ tests/modules/programs/jq/null-package.nix
@@ -7,7 +7,7 @@
 
     nmt.script = ''
       # With null package, jq should not be added to home.packages
-      assertFileNotRegex home-path/etc/profiles/per-user/${config.home.username}/bin/jq ''
+      assertPathNotExists home-path/bin/jq
     '';
   };
 }
