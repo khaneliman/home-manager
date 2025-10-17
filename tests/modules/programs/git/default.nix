f36e0798cbf062235cd6924da68cc61ba79d4b08
Austin Horstman <khaneliman12@gmail.com>
2025-10-16 21:11:02 -0500

    patdiff: new module
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

--- tests/modules/programs/git/default.nix
+++ tests/modules/programs/git/default.nix
@@ -9,5 +9,4 @@
   git-without-signing = ./git-without-signing.nix;
   git-with-hooks = ./git-with-hooks.nix;
   git-with-maintenance = ./git-with-maintenance.nix;
-  git-patdiff = ./git-patdiff.nix;
 }
