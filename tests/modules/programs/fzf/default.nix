9ed13f1f945ca1008b20a3620b70836c73ee6082
Austin Horstman <khaneliman12@gmail.com>
2025-11-03 23:49:13 -0600

    WIP
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

--- tests/modules/programs/fzf/default.nix
+++ tests/modules/programs/fzf/default.nix
@@ -5,5 +5,4 @@
   fzf-tmux-integration = ./tmux-integration.nix;
   fzf-widget-commands = ./widget-commands.nix;
   fzf-zsh-integration = ./zsh-integration.nix;
-  fzf-null-package = ./null-package.nix;
 }
