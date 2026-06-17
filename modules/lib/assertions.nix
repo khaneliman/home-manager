ff986729c6de764c2e17fda620056952da908a5c
Austin Horstman <khaneliman12@gmail.com>
2026-06-17 13:24:27 -0500

    lib: support optionPath and return relatedOptions in assertPlatform
    Allow assertPlatform to take a detailed module attribute set with custom optionPath, and return relatedOptions for definition location lookup.

--- modules/lib/assertions.nix
+++ modules/lib/assertions.nix
@@ -1,15 +1,31 @@
 { lib }:
 
 {
-  assertPlatform = module: pkgs: platforms: {
-    assertion = lib.elem pkgs.stdenv.hostPlatform.system platforms;
-    message =
-      let
-        platformsStr = lib.concatStringsSep "\n" (map (p: "  - ${p}") (lib.sort (a: b: a < b) platforms));
-      in
-      ''
-        The module ${module} does not support your platform. It only supports
+  assertPlatform =
+    moduleOrAttrs: pkgs: platforms:
+    let
+      attrs =
+        if lib.isAttrs moduleOrAttrs then
+          moduleOrAttrs
+        else
+          {
+            module = moduleOrAttrs;
+          };
+
+      optionPath = attrs.optionPath or (lib.splitString "." attrs.module);
+      relatedOptions =
+        attrs.relatedOptions or [
+          (optionPath ++ [ "enable" ])
+          optionPath
+        ];
+      platformsStr = lib.concatStringsSep "\n" (map (p: "  - ${p}") (lib.sort (a: b: a < b) platforms));
+    in
+    {
+      assertion = lib.elem pkgs.stdenv.hostPlatform.system platforms;
+      inherit relatedOptions;
+      message = ''
+        The module ${attrs.module} does not support your platform. It only supports
 
         ${platformsStr}'';
-  };
+    };
 }
